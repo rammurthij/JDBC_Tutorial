0583b85f13e722498040d026d2e5644c18d9d042
HP <rammurthijalukuri@gmail.com>
2022-07-15 10:32:19 +0530

    modified file JDBC

--- JDBC_Concept/src/JDBC_Practice/JDBC_demo.java
+++ JDBC_Concept/src/JDBC_Practice/JDBC_demo.java
@@ -3,6 +3,7 @@ package JDBC_Practice;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class JDBC_demo {
 
@@ -15,11 +16,18 @@ public class JDBC_demo {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
 		Connection con=DriverManager.getConnection(url,name,password);
-		String query="create table vikram(name varchar(20),address varchar(20),location varchar(20))";
+		//String query="create table vikram(name varchar(20),address varchar(20),location varchar(20))";
+		String query="select* from vikram";
 		PreparedStatement ps=con.prepareStatement(query);
 		
-		ps.executeUpdate();
-		System.out.println("Table created Successfully");
+		//ps.executeUpdate("insert into vikram values('Kamal Hassan','chennai','Tamilnadu')");
+		//ps.executeUpdate("insert into vikram values('Vijay Sethupathi','chennai','Tamilnadu')");
+		//System.out.println("Values are inserted");
+		//System.out.println("Table created Successfully");
+		ResultSet rs=ps.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getString(1)+" "+ rs.getString(2)+" "+rs.getString(3));
+		}
 		
 		con.close();
 		
